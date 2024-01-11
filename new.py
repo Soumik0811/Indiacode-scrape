5e26d7cc57f5a08bb9a6386bca281def52dd5b25
Soumik0811 <91133940+Soumik0811@users.noreply.github.com>
2024-01-11 20:03:22 +0530

    Update new.py

--- new.py
+++ new.py
@@ -1,51 +1,22 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from bs4 import BeautifulSoup
-# import requests
-# import csv
-# from selenium.common.exceptions import NoSuchElementException
-# from webdriver_manager.chrome import ChromeDriverManager
-# import time
-# from selenium.webdriver.chrome.service import Service as ChromeService
-# from selenium.webdriver.chrome.service import Service
-# import streamlit as st
-# import pandas as pd
-# import os
-# from selenium.webdriver.chrome.options import Options
-
-
-
-# def extract(url):
-# # Initialize an empty list to store scraped data
-#     data_list = []
-#     chrome_version = "120.0.6099.200"  # Replace with your actual Chrome version
-#     chrome_options = Options()
-#     chrome_options.add_argument("--headless")
-#     chrome_options.add_argument('--disable-gpu')
-#     service = ChromeService(executable_path=ChromeDriverManager(chrome_version).install())
-#     driver = webdriver.Chrome(service=service, options=chrome_options)
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+import requests
 import csv
 from selenium.common.exceptions import NoSuchElementException
-from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 import time
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.chrome.service import Service
 import streamlit as st
 import pandas as pd
 import os
-
 def extract(url):
-    # Initialize an empty list to store scraped data
+# Initialize an empty list to store scraped data
     data_list = []
-    # firefox_options = FirefoxOptions()
-    # firefox_options.add_argument("--headless")
-    # service = FirefoxService(executable_path=GeckoDriverManager().install())
-    # driver = webdriver.Firefox(service=service, options=firefox_options)
-    driver = webdriver.Firefox(GeckoDriverManager().install())   
+    service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
+    # driver = webdriver.Chrome(ChromeDriverManager().install()) 
     driver.get(url)  
     response = requests.get(url)
     if response.status_code == 200:
@@ -178,8 +149,10 @@ def extract(url):
             list_links.append('NA')
     except NoSuchElementException:
         list_links.append('NA')
+
     
     scraped_data = list(scraped.items())
+    # print(data_dict)
     print("act pdf:", act_pdf)
     print("Rule PDF", list_links)
     print("Regulation PDF", regu_links)
@@ -214,80 +187,6 @@ def extract(url):
         "Link": url
     }
     return data
-#     # Append the data dictionary to the list
-#     data_list.append(data)
-
-# # Specify the CSV file name and column headers
-#     csv_file = "product.csv"
-#     headers = ["Act Details","Act ID", "Act Number","Enactment Date", "Act Year", "Short Title", "Ministry","Department","Type","Location", "Act PDF","Rule PDF","Regulation PDF","Notification PDF","Circular PDF","Order PDF","Statutes PDF","Ordinance PDF","Link"]  # Add more headers for additional fields
-
-# # Write the data to the CSV file
-#     with open(csv_file, 'a', newline='', encoding='utf-8') as file:
-#         writer = csv.DictWriter(file, fieldnames=headers)
-#         # writer.writeheader()  # Write header row
-#         writer.writerows(data_list)  # Write data rows
-#         print("Data saved to product.csv")
-
-url_links= '''https://www.indiacode.nic.in/handle/123456789/18988?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18993?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18996?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18989?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18990?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18994?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18995?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18985?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19012?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19014?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19016?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19018?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19019?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18987?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18983?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18984?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19008?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19009?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19002?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19022?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19023?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19017?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19020?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18992?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18991?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18986?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18998?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19001?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19000?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19003?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19004?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18982?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18981?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18999?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19015?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19010?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19011?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19025?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19021?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19026?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/18997?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19024?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19013?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19006?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19007?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19005?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19318?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19320?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19319?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19315?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19150?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19151?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19251?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19313?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19314?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19316?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19311?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19317?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19312?view_type=search&sam_handle=123456789/2454
-https://www.indiacode.nic.in/handle/123456789/19582?view_type=search&sam_handle=123456789/2454'''
 
     
 def main():
