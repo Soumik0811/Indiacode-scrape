35a5f618c01600e8e67886bc43053ea82edd91d9
Soumik0811 <91133940+Soumik0811@users.noreply.github.com>
2024-01-11 18:22:22 +0530

    Update new.py

--- new.py
+++ new.py
@@ -13,10 +13,10 @@ import os
 def extract(url):
 # Initialize an empty list to store scraped data
     data_list = []
-    # service = Service(ChromeDriverManager().install())
-    # options = webdriver.ChromeOptions()
-    # driver = webdriver.Chrome(service=service, options=options)
-    driver = webdriver.Chrome(ChromeDriverManager().install()) 
+    service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
+    # driver = webdriver.Chrome(ChromeDriverManager().install()) 
     driver.get(url)  
     response = requests.get(url)
     if response.status_code == 200:
@@ -181,6 +181,81 @@ def extract(url):
         "Link": url
     }
     return data
+#     # Append the data dictionary to the list
+#     data_list.append(data)
+
+# # Specify the CSV file name and column headers
+#     csv_file = "product.csv"
+#     headers = ["Act Details","Act ID", "Act Number","Enactment Date", "Act Year", "Short Title", "Ministry","Department","Type","Location", "Act PDF","Rule PDF","Regulation PDF","Notification PDF","Circular PDF","Order PDF","Statutes PDF","Ordinance PDF","Link"]  # Add more headers for additional fields
+
+# # Write the data to the CSV file
+#     with open(csv_file, 'a', newline='', encoding='utf-8') as file:
+#         writer = csv.DictWriter(file, fieldnames=headers)
+#         # writer.writeheader()  # Write header row
+#         writer.writerows(data_list)  # Write data rows
+#         print("Data saved to product.csv")
+
+url_links= '''https://www.indiacode.nic.in/handle/123456789/18988?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18993?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18996?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18989?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18990?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18994?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18995?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18985?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19012?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19014?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19016?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19018?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19019?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18987?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18983?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18984?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19008?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19009?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19002?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19022?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19023?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19017?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19020?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18992?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18991?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18986?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18998?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19001?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19000?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19003?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19004?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18982?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18981?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18999?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19015?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19010?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19011?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19025?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19021?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19026?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/18997?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19024?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19013?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19006?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19007?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19005?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19318?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19320?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19319?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19315?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19150?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19151?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19251?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19313?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19314?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19316?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19311?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19317?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19312?view_type=search&sam_handle=123456789/2454
+https://www.indiacode.nic.in/handle/123456789/19582?view_type=search&sam_handle=123456789/2454'''
+
     
 def main():
     st.title("Web Scraping with Streamlit")
@@ -190,45 +265,47 @@ def main():
     urls = url_input.split('\n')
 
     if st.button("Scrape Data"):
+        new=[]
         all_data = []
         for i, url in enumerate(urls):
             if url.strip() != "":
                 st.write(f"Scraping data from URL {i + 1}: {url}")
                 data = extract(url)
-                all_data.append(data)
+                new.append(data)
+                all_data.extend(new) 
 
         if all_data:
             headers = ["Act Details", "Act ID", "Act Number", "Enactment Date", "Act Year", "Short Title", "Ministry", "Department", "Type", "Location", "Act PDF", "Rule PDF", "Regulation PDF", "Notification PDF", "Circular PDF", "Order PDF", "Statutes PDF", "Ordinance PDF", "Link"]
             csv_filename = "new_scraped.csv"
-
             downloads_dir = os.path.join(os.path.expanduser("~"), "Downloads")
             csv_file_path = os.path.join(downloads_dir, csv_filename)
 
-    # Check if the file exists
-            if not os.path.exists(downloads_dir):
-                os.makedirs(downloads_dir)
-
-    # Check if the CSV file exists to write the header
+            # Check if the CSV file exists to write the header
             file_exists = os.path.isfile(csv_file_path)
             if not file_exists:
                 with open(csv_file_path, 'w', newline='', encoding='utf-8') as file:
                     writer = csv.DictWriter(file, fieldnames=headers)
                     writer.writeheader()
 
-    # Append data to the CSV file
+            # Append data to the CSV file
             with open(csv_file_path, 'a', newline='', encoding='utf-8') as file:
                 writer = csv.DictWriter(file, fieldnames=headers)
                 writer.writerows(all_data)  # Write data rows
 
             st.success("Data scraped successfully!")
 
-    # Download button
+            # Read the file content
+            with open(csv_file_path, "rb") as file:
+                csv_file_content = file.read()
+
+            # Download button
             st.download_button(
-            label="Download new_scraped.csv",
-            data=open("new_scraped.csv", "rb"),
-            file_name="new_scraped.csv",
-            mime="text/csv",
-        )
+                label="Download new_scraped.csv",
+                data=csv_file_content,
+                file_name="new_scraped.csv",
+                mime="text/csv",
+            )
 
 if __name__ == "__main__":
     main()
+
